979e519cab1498ac9cce8232965d0f6023e582da
Matteo Convertino <mconvertino@deloitte.it>
2022-10-07 08:39:21 +0000

    init template

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["btp/academy/customers/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"btpacademy/customers/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
